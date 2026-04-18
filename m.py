4e6c074ba3b5452e2577015a9faa4107f208df9b
nitish05584 <devnath8471@gmail.com>
2026-04-18 22:11:06 +0530

    m

---
+++ m.py
@@ -0,0 +1,15 @@
+string1="Hello"
+print('h' in string1) # True
+print('H' in string1) # True
+print('e' in string1) # True
+print('o' in string1) # True
+print('l' in string1) # True  
+
+l=[1, 2, 3, 4, 5]
+print(1 in l) # True
+print(6 not in l)
+# True  
+d={'a':1, 'b':2, 'c':3}
+print('a' in d) # True
+print('d' in d) # False 
+
