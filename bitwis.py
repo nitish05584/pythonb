ac37d5b3e7191e334141150933ad778ec9a980bd
nitish05584 <devnath8471@gmail.com>
2026-04-18 22:24:23 +0530

    bitwise

---
+++ bitwis.py
@@ -0,0 +1,14 @@
+x=10
+y=20
+print(x & y)
+print(x | y)
+print(x ^ y)
+print(~x)
+print(x << 2)
+print(x >> 2)
+print(bin(x))
+print(bin(y))
+print(bin(x & y))
+print(bin(x | y))
+print(bin(x ^ y))
+    
