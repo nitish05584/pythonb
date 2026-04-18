2aea639a83e4cd2ddb8474dbcc8c017ed69a23a6
nitish05584 <devnath8471@gmail.com>
2026-04-18 23:22:42 +0530

    unip

---
+++ uniput.py
@@ -0,0 +1,16 @@
+a=input("Enter a number: ")
+b=input("Enter another number: ")
+print(a)
+print(b)
+
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+print(a+b)
+
+a=float(input("Enter a number: "))
+b=float(input("Enter another number: "))
+print(a+b)
+
+a=eval(input("Enter a number: "))
+b=eval(input("Enter another number: "))
+print(a+b)
