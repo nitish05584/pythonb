f03a628cc77496b70c7985347ff28cafdf216738
nitish05584 <devnath8471@gmail.com>
2026-04-18 23:40:51 +0530

    cond

---
+++ cond2.py
@@ -0,0 +1,12 @@
+per=int(input("Enter your percentage: "))
+if per>=90:
+    print("Grade A")
+elif per>=80:
+    print("Grade B")
+elif per>=70:
+    print("Grade C")
+elif per>=60:
+    print("Grade D")
+else:
+    print("Grade F")
+    
