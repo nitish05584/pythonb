e71d85080b0968f0c4b3d42de1a77806e8b3fbc2
nitish05584 <devnath8471@gmail.com>
2026-04-19 18:19:51 +0530

    nest

---
+++ netsdict.py
@@ -0,0 +1,7 @@
+d={
+    'php':{"duration":'2months','fees':2000},
+    'python':{"duration":'3months','fees':3000},
+    'java':{"duration":'4months','fees':4000},
+}
+print(d['php']['duration'])
+print(d['python']['fees'])
