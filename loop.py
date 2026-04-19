1c8e392690ec7d9d6c02b25dd7d5dbb8c5e8ec50
nitish05584 <devnath8471@gmail.com>
2026-04-19 11:35:40 +0530

    lop

---
+++ loop.py
@@ -0,0 +1,9 @@
+for i in range(5):
+    print(i)
+for i in range(1, 10, 2):
+    print(i)
+for i in range(10, 0, -1):
+    print(i)    
+for i in range(1,40):
+    print(i)        
+                  
