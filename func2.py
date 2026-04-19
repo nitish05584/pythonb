78f0fbad7cc58b7bdd9fff913390a2f542d91fb6
nitish05584 <devnath8471@gmail.com>
2026-04-19 22:49:42 +0530

    func

---
+++ func2.py
@@ -0,0 +1,12 @@
+def showdata():
+    print("This is a function to show data.")
+showdata()
+
+def sum(a, b):
+    return a + b
+result = sum(5, 10)
+print("The sum is:", result)
+
+def greet(a,b):
+    print(a+b)
+greet(4, 6)
