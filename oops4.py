63fcbecb6d638c9f217f386d885922a019a4a8f0
nitish05584 <devnath8471@gmail.com>
2026-04-20 17:51:00 +0530

    oops

---
+++ oops4.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age) 
+        
+obj = Student("Alice", 20)
+obj.display()        
