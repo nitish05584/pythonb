3a149b767f19ecc02f0dacf05eb66615a61121a8
nitish05584 <devnath8471@gmail.com>
2026-04-20 13:52:01 +0530

    random

---
+++ rand.py
@@ -0,0 +1,20 @@
+import random
+
+n=random.randint(1,100)
+print(n)
+
+n1=random.randrange(1,100)
+print(n1)
+
+l=[1,2,3,4,5]
+print(random.choice(l))
+
+r=random.random()
+print(r)
+
+l=[1,2,3,4,5]
+random.shuffle(l)
+print(l)
+
+
+
