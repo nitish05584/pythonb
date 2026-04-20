e1558b538ed354fa06edb69e9787662b3c77dfca
nitish05584 <devnath8471@gmail.com>
2026-04-20 13:43:52 +0530

    mat

---
+++ mat.py
@@ -0,0 +1,21 @@
+import math
+x=12
+print(math.ceil(x*5))
+
+x=-10
+print(math.fabs(x))
+
+x=5
+print(math.factorial(x))
+
+x=11.5
+print(math.floor(x))
+
+l=[1,2,3,4,5]
+print(math.fsum(l))
+
+x=2
+print(math.pow(x,3))
+
+x=10
+print(math.sqrt(x))
